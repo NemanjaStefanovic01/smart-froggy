8154723e5be753a1669978d979c1e62ba7ab7c0d
Nemanja Stefanovic <MuseGr>
2022-07-17 16:51:14 +0200

    added frog and rotating fly, ready to start coding path finding

--- script.js
+++ script.js
@@ -19,23 +19,58 @@ for(var i = 0; i < pondSize; i++){
     pond.appendChild(space)
 }
 
+
     //ADD MOVABLE SPACES
 const spacesList = document.querySelectorAll('.space')
 
 //hover
 function select(){
-    if(!event.target.classList.contains('selected')){
+    const space = event.target.parentElement
+
+    if(!space.classList.contains('selected') && !space.classList.contains('gameObject')){
         event.target.src = 'images/waterLilyHover.png'
     }
 }
 function removeSelection(){
-    if(!event.target.classList.contains('selected')){
+    const space = event.target.parentElement
+    if(!space.classList.contains('selected') && !space.classList.contains('gameObject')){
         event.target.src = 'images/water.png'
     }
 }
 
 //place
 function placeLily(){
-    event.target.src = 'images/waterLily.png'
-    event.target.classList.add('selected')
+    const space = event.target.parentElement
+    if(!space.classList.contains('selected') && !space.classList.contains('gameObject')){
+        event.target.src = 'images/waterLily.png'
+        space.classList.add('selected')
+    }    
 }
+
+
+    //ADD FROG AND FLY
+//frog
+let frogPosition = Math.floor(Math.random() * ((8*8)+1))
+
+const frog = document.createElement('img')
+frog.src = "images/froggy.png"
+frog.classList.add('frog')
+
+spacesList[frogPosition].appendChild(frog)
+spacesList[frogPosition].classList.add('gameObject')
+spacesList[frogPosition].children[0].src = 'images/waterLilyHover.png'
+
+//fly
+let flyPosition = Math.floor(Math.random() * ((8*8)+1))
+
+while(Math.abs(flyPosition - frogPosition) < 30){
+    flyPosition = Math.floor(Math.random() * ((8*8)+1))
+}
+
+const fly = document.createElement('img')
+fly.src = "images/fly.png"
+fly.classList.add('fly')
+fly.classList.add('rotate')
+
+spacesList[flyPosition].appendChild(fly)
+spacesList[flyPosition].classList.add('gameObject')
